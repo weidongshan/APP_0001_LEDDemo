af50e2af4d7cf3af46d5b6e615e82d37555957f7
weidongshan <weidongshan@qq.com>
2015-08-17 18:53:03 +0800

    v1, created by android studio

---
+++ app/src/androidTest/java/com/thisway/app_0001_leddemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.thisway.app_0001_leddemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
